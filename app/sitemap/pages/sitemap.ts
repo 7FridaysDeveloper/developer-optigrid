1f326edf9aec2f826cb71430f99f2d59335882cf
7FridaysDeveloper <108727315+7FridaysDeveloper@users.noreply.github.com>
2025-09-22 10:27:14 +0300

    Update sitemap.ts

--- app/sitemap/pages/sitemap.ts
+++ app/sitemap/pages/sitemap.ts
@@ -11,7 +11,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             priority: 1.0,
         },
         {
-            url: `${baseUrl}/about/`,
+            url: `${baseUrl}/about-us/`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
